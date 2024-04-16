2e68db946094d976f3104e3aa78f24afd050a4c8
Pietro Flemming <pietroflemming@gmail.com>
2024-04-16 10:00:46 -0300

    Adicionando Tutoriais vue

---
+++ vue/intro-to-vue-3_tutorial-2/main.js
@@ -0,0 +1,10 @@
+// const app = Vue.createApp({Options Object})
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'A well-designed pair of sock',
+        }
+    }
+})
